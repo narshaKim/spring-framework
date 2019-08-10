8d13e8722d132d212b658f12ca6441c03d751852
narshaKim <narsha9501@gmail.com>
2019-08-10 16:25:33 +0900

    1 오브젝트와 의존관계 1.5 스프링의 IoC 1.5.1 오브젝트 팩토리를 이용한 스프링 IoC

--- spring/src/main/java/UserDaoTest.java
+++ spring/src/main/java/UserDaoTest.java
@@ -1,6 +1,8 @@
 import component.DaoFactory;
 import dao.UserDao;
 import domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
@@ -8,7 +10,8 @@ public class UserDaoTest {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
         user.setId("narsha");
--- spring/src/main/java/component/DaoFactory.java
+++ spring/src/main/java/component/DaoFactory.java
@@ -3,14 +3,18 @@ package component;
 import dao.ConnectionMaker;
 import dao.DConnectionMaker;
 import dao.UserDao;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DaoFactory {
 
+    @Bean
     public UserDao userDao() {
-        UserDao userDao = new UserDao(connectionMaker());
-        return userDao;
+        return new UserDao(connectionMaker());
     }
 
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
