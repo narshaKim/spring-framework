ed3076503a932e3237ddb884246173a101f24f81
narshaKim <narsha9501@gmail.com>
2019-08-10 15:29:04 +0900

    1 오브젝트와 의존관계 1.1 초난감 DAO 1.1.1 User 1.1.2 UserDao 1.1.3 main()을 이용한 DAO 테스트 코드

---
+++ spring/src/main/java/Main.java
@@ -0,0 +1,24 @@
+import dao.UserDao;
+import domain.User;
+
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("narsha");
+        user.setName("김솔잎");
+        user.setPassword("solip");
+        dao.add(user);
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+
+}
