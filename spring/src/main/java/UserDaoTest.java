adc080a13d0e56ff3f36309e9aa4a2167a3f83d8
narshaKim <narsha9501@gmail.com>
2019-08-10 16:38:35 +0900

    1 오브젝트와 의존관계 1.8 XML을 이용한 설정 1.8.1 XML 설정 1.8.2 XML을 이용하는 애플리케이션 컨텍스트

--- spring/src/main/java/UserDaoTest.java
+++ spring/src/main/java/UserDaoTest.java
@@ -1,8 +1,7 @@
-import component.DaoFactory;
 import dao.UserDao;
 import domain.User;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
@@ -10,7 +9,7 @@ public class UserDaoTest {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext context = new GenericXmlApplicationContext("/spring-config.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
