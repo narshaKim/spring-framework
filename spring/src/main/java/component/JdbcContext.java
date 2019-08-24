d10654db3bc0dae69fa9955bec9f5b10083218e6
narshaKim <narsha9501@gmail.com>
2019-08-24 18:08:59 +0900

    3 템플릿 3.5 템플릿과 콜백 3.5.2 편리한 콜백의 재활용

--- spring/src/main/java/component/JdbcContext.java
+++ spring/src/main/java/component/JdbcContext.java
@@ -71,4 +71,36 @@ public class JdbcContext {
         }
     }
 
+    public void executeSql(final String sql, final Object... items) throws SQLException {
+        workWithStatementStrategy(getStrategy(sql, items));
+    }
+
+    public <T> T executeSql(ResultSetStrategy resultSetStrategy, final String sql, final Object... items) throws SQLException {
+        return workWithResultSetStrategy(getStrategy(sql, items), resultSetStrategy);
+    }
+
+    private StatementStrategy getStrategy(final String sql, final Object[] items) throws SQLException {
+        return new StatementStrategy() {
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement(sql);
+                for(int i=0; i<items.length; i++) {
+                    Object item = items[i];
+                    if(item instanceof Integer) {
+                        ps.setInt(i+1, (Integer) item);
+                    } else if (item instanceof Long) {
+                        ps.setLong(i+1, (Long) item);
+                    } else if (item instanceof Float) {
+                        ps.setFloat(i+1, (Float) item);
+                    } else if (item instanceof Double) {
+                        ps.setDouble(i+1, (Double) item);
+                    } else if (item instanceof Boolean) {
+                        ps.setBoolean(i+1, (Boolean) item);
+                    } else if (item instanceof String) {
+                        ps.setString(i+1, (String) item);
+                    }
+                }
+                return ps;
+            }
+        };
+    }
 }
--- spring/src/main/java/dao/UserDao.java
+++ spring/src/main/java/dao/UserDao.java
@@ -4,10 +4,7 @@ import component.JdbcContext;
 import domain.User;
 import org.springframework.dao.EmptyResultDataAccessException;
 import strategy.ResultSetStrategy;
-import strategy.StatementStrategy;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -20,60 +17,41 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        jdbcContext.workWithStatementStrategy(new StatementStrategy() {
-            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                return c.prepareStatement("DELETE FROM USERS");
-            }
-        });
+        jdbcContext.executeSql("DELETE FROM USERS");
     }
 
     public void add(final User user) throws SQLException {
-        jdbcContext.workWithStatementStrategy(new StatementStrategy() {
-            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement("INSERT INTO USERS(ID, NAME, PASSWORD) VALUES(?,?,?)");
-                ps.setString(1, user.getId());
-                ps.setString(2, user.getName());
-                ps.setString(3, user.getPassword());
-                return ps;
-            }
-        });
+        jdbcContext.executeSql("INSERT INTO USERS(ID, NAME, PASSWORD) VALUES(?,?,?)", user.getId(), user.getName(), user.getPassword());
     }
 
     public int getCount() throws SQLException {
-        return (Integer) jdbcContext.workWithResultSetStrategy(new StatementStrategy() {
-            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                return c.prepareStatement("SELECT COUNT(*) FROM USERS");
-            }
-        }, new ResultSetStrategy() {
+        ResultSetStrategy resultSetStrategy = new ResultSetStrategy() {
             public <T> T getResult(ResultSet rs) throws SQLException {
                 rs.next();
                 return (T) (Integer) rs.getInt(1);
             }
-        });
+        };
+
+        return jdbcContext.executeSql(resultSetStrategy, "SELECT COUNT(*) FROM USERS");
     }
 
     public User get(final String id) throws SQLException, ClassNotFoundException {
-        return (User)jdbcContext.workWithResultSetStrategy(new StatementStrategy() {
-            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement("SELECT * FROM USERS WHERE ID=?");
-                ps.setString(1, id);
-                return ps;
-            }
-        }, new ResultSetStrategy() {
+        ResultSetStrategy resultSetStrategy = new ResultSetStrategy() {
             public <T> T getResult(ResultSet rs) throws SQLException {
                 User user = null;
-                if(rs.next()) {
+                if (rs.next()) {
                     user = new User();
                     user.setId(rs.getString("ID"));
                     user.setName(rs.getString("NAME"));
                     user.setPassword(rs.getString("PASSWORD"));
                 }
-                if(user==null) {
+                if (user == null) {
                     throw new EmptyResultDataAccessException(1);
                 }
                 return (T) user;
             }
-        });
-    }
+        };
 
+        return jdbcContext.executeSql(resultSetStrategy, "SELECT * FROM USERS WHERE ID=?", id);
+    }
 }
