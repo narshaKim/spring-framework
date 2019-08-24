ba6dc681cc567ed81a3cbe49c7db1451b5383cb6
narshaKim <narsha9501@gmail.com>
2019-08-24 15:47:22 +0900

    2 테스트 2.3 개발자를 위한 테스팅 프레임워크 JUnit 2.3.5 테스트 코드 개선

--- spring/src/main/java/UserDaoTest.java
+++ spring/src/main/java/UserDaoTest.java
@@ -2,6 +2,7 @@ import dao.UserDao;
 import domain.User;
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -11,14 +12,24 @@ import java.sql.SQLException;
 
 public class UserDaoTest {
 
+    private UserDao dao;
+
+    private User user1;
+    private User user2;
+    private User user3;
+
+    @Before
+    public void setUp() {
+        ApplicationContext context = new GenericXmlApplicationContext("/spring-config.xml");
+        this.dao = context.getBean("userDao", UserDao.class);
+
+        this.user1 = new User("bts-jk", "전정국", "jk");
+        this.user2 = new User("bts-rm", "김남준", "rm");
+        this.user3 = new User("bts-v", "김태형", "v");
+    }
+
     @Test
     public void addAndGet() throws SQLException, ClassNotFoundException {
-
-        ApplicationContext context = new GenericXmlApplicationContext("/spring-config.xml");
-        UserDao dao = context.getBean("userDao", UserDao.class);
-
-        User user1 = new User("bts-jk", "전정국", "jk");
-        User user2 = new User("bts-v", "김태형", "v");
 
         dao.deleteAll();
         Assert.assertThat(dao.getCount(), CoreMatchers.is(0));
@@ -40,9 +51,6 @@ public class UserDaoTest {
     @Test(expected = EmptyResultDataAccessException.class)
     public void getUserFailure() throws SQLException, ClassNotFoundException {
 
-        ApplicationContext context = new GenericXmlApplicationContext("/spring-config.xml");
-        UserDao dao = context.getBean("userDao", UserDao.class);
-
         dao.deleteAll();
         Assert.assertThat(dao.getCount(), CoreMatchers.is(0));
 
@@ -52,12 +60,6 @@ public class UserDaoTest {
 
     @Test
     public void count() throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new GenericXmlApplicationContext("/spring-config.xml");
-
-        UserDao dao = context.getBean("userDao", UserDao.class);
-        User user1 = new User("bts-jk", "전정국", "jk");
-        User user2 = new User("bts-rm", "김남준", "rm");
-        User user3 = new User("bts-v", "김태형", "v");
 
         dao.deleteAll();
         Assert.assertThat(dao.getCount(), CoreMatchers.is(0));
