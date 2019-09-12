53c2ca6bdda39dda37ca90e2d848859b670b2dd6
narshaKim <narsha9501@gmail.com>
2019-09-12 22:15:09 +0900

    6 AOP 6.4 스프링의 프록시 팩토리 빈 6.4.2 ProxyFactoryBean 적용

---
+++ spring/src/main/java/Proxy/TransactionAdvice.java
@@ -0,0 +1,28 @@
+package Proxy;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+public class TransactionAdvice implements MethodInterceptor {
+
+    PlatformTransactionManager transactionManager;
+
+    public void setTransactionManager(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+    public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            Object ret = methodInvocation.proceed();
+            this.transactionManager.commit(status);
+            return ret;
+        } catch (RuntimeException e) {
+            this.transactionManager.rollback(status);
+            throw e;
+        }
+    }
+}
