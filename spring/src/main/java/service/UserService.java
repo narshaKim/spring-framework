4a17ad374af6579813ba19025cba6811a3012344
narshaKim <narsha9501@gmail.com>
2019-09-15 12:22:24 +0900

    6 AOP 6.7 애노테이션 트랜잭션 속성과 포인트컷 6.7.2 트랜잭션 애노테이션 적용

--- spring/src/main/java/service/UserService.java
+++ spring/src/main/java/service/UserService.java
@@ -1,12 +1,16 @@
 package service;
 
 import domain.User;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
 public interface UserService {
     void add(User user);
+    @Transactional(readOnly = true)
     User get(String id);
+    @Transactional(readOnly = true)
     List<User> getAll();
     void deleteAll();
     void update(User user);
