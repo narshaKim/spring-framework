63f07fa5256ae1c4a14ace0ae9682c87a86a1a5b
narshaKim <narsha9501@gmail.com>
2019-09-29 13:26:21 +0900

    7 스프링 핵심 기술의 응용 7.3 서비스 추상화 적용 7.3.1 OXM 서비스 추상화

---
+++ spring/src/main/java/OxmTest.java
@@ -0,0 +1,41 @@
+import JAXB.SqlType;
+import JAXB.Sqlmap;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/applicationContext.xml")
+public class OxmTest {
+
+    @Autowired
+    Unmarshaller unmarshaller;
+
+    @Test
+    public void unmarshallerSqlMap() throws IOException {
+        Source xmlSource = new StreamSource(getClass().getResourceAsStream("/jaxbtest.xml"));
+        Sqlmap sqlmap = (Sqlmap) this.unmarshaller.unmarshal(xmlSource);
+
+        List<SqlType> sqlList = sqlmap.getSql();
+        Assert.assertThat(sqlList.size(), CoreMatchers.is(3));
+        Assert.assertThat(sqlList.get(0).getKey(), CoreMatchers.is("add"));
+        Assert.assertThat(sqlList.get(0).getValue(), CoreMatchers.is("insert"));
+        Assert.assertThat(sqlList.get(1).getKey(), CoreMatchers.is("get"));
+        Assert.assertThat(sqlList.get(1).getValue(), CoreMatchers.is("select"));
+        Assert.assertThat(sqlList.get(2).getKey(), CoreMatchers.is("delete"));
+        Assert.assertThat(sqlList.get(2).getValue(), CoreMatchers.is("delete"));
+
+
+    }
+
+}
