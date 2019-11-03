d3127d9a012c905aa1144e6c9ba3ea72a736dc9a
narshaKim <narsha9501@gmail.com>
2019-11-03 16:24:53 +0900

    7 스프링 핵심 기술의 응용 7.6 스프링 3.1의 DI 7.6.5 프로퍼티 소스 (1)

--- spring/src/main/java/AppContext.java
+++ spring/src/main/java/AppContext.java
@@ -1,5 +1,7 @@
 import component.DummyMailSender;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.mail.MailSender;
@@ -9,21 +11,32 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import service.UserService;
 
 import javax.sql.DataSource;
+import java.sql.Driver;
 
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages="dao")
+@PropertySource("/database.properties")
 @Import({SqlServiceContext.class})
 public class AppContext {
+
+    @Autowired
+    private Environment env;
 
     /** DB */
     @Bean
     public DataSource dataSource() {
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-        dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
-        dataSource.setUrl("jdbc:mysql://localhost/solip_spring");
-        dataSource.setUsername("root");
-        dataSource.setPassword("root");
+        try {
+            dataSource.setDriverClass(
+                    (Class<? extends Driver>) Class.forName(env.getProperty("db.driverClass"))
+            );
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.username"));
+        dataSource.setPassword(env.getProperty("db.password"));
         return dataSource;
     }
 
